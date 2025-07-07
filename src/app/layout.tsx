a8fc2cf1bb451e863a15f562f02755ee0258c523
muhazAhmed <muhazvla313@gmail.com>
2025-07-07 19:19:42 +0530

    fix: some minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-[#ecfef3]">{children}</body>
     </html>
   );
 }
