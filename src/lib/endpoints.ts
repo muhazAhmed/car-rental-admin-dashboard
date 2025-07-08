ffd14c3aef3290e29b514f3d75892b1516a17304
muhazAhmed <muhazvla313@gmail.com>
2025-07-08 09:20:07 +0530

    chore: added custom api call

---
+++ src/lib/endpoints.ts
@@ -0,0 +1,4 @@
+export const endpoints = {
+    cars: "/cars",
+    summary: "/summary",
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import axios from "axios";
+
+const baseURL = process.env.API_URL ?? "";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export async function CustomAxios<T>(endpoint: string): Promise<T> {
+  try {
+    const url = `${baseURL}${endpoint}`;
+    const response = await axios.get<T>(url, { headers: { "Cache-Control": "no-store" } });
+    return response.data;
+  } catch (error) {
+    console.error(`❌ Error fetching from ${endpoint}:`, error);
+    throw error;
+  }
 }
